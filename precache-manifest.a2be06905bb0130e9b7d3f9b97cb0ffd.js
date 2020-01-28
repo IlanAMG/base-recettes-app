038a2421d035168cff89aac547bd550d8a323ae9
Ilan Amzallag <Ilan@MacBook-Air-de-Amzallag.local>
2020-01-28 16:39:04 +0100

    Updates

---
+++ precache-manifest.a2be06905bb0130e9b7d3f9b97cb0ffd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8c554871e8e16cd4ac9",
+    "url": "/base-recettes-app/static/css/main.3069aea5.chunk.css"
+  },
+  {
+    "revision": "a8c554871e8e16cd4ac9",
+    "url": "/base-recettes-app/static/js/main.a8c55487.chunk.js"
+  },
+  {
+    "revision": "49098ee8b58167a7a42e",
+    "url": "/base-recettes-app/static/js/1.49098ee8.chunk.js"
+  },
+  {
+    "revision": "8427089918518d29a8d6",
+    "url": "/base-recettes-app/static/js/runtime~main.84270899.js"
+  },
+  {
+    "revision": "b9a86c50cc3fe786eb584dd4dc784caf",
+    "url": "/base-recettes-app/static/media/default.b9a86c50.jpeg"
+  },
+  {
+    "revision": "4200b6b96b6c171e52c4a97d34a1174e",
+    "url": "/base-recettes-app/static/media/chocolat.4200b6b9.jpeg"
+  },
+  {
+    "revision": "815852b98cfd976880da9f88ecc5892e",
+    "url": "/base-recettes-app/static/media/epinard.815852b9.jpg"
+  },
+  {
+    "revision": "fca17319421519cbb9d73c85b4879566",
+    "url": "/base-recettes-app/static/media/hamburger.fca17319.jpeg"
+  },
+  {
+    "revision": "287d53ae5223bf74627baa68ecc5f5c1",
+    "url": "/base-recettes-app/static/media/pizza.287d53ae.jpeg"
+  },
+  {
+    "revision": "8324a6142c5fc9d0bdd0b453c162f9a1",
+    "url": "/base-recettes-app/static/media/saumon.8324a614.jpeg"
+  },
+  {
+    "revision": "96c2f7548bab68d96c489276dbc837e6",
+    "url": "/base-recettes-app/static/media/tartine.96c2f754.jpg"
+  },
+  {
+    "revision": "b42a81c98e1dc1f2165357c4cb93d2af",
+    "url": "/base-recettes-app/index.html"
+  }
+];
